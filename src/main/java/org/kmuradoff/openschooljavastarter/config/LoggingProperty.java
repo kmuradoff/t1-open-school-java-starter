06527c45368afb0f9f331e0c0f2b03d6457f0ba6
Kamal Muradov <muradoffk@gmail.com>
2025-03-08 01:11:18 +0300

    Implemented starter

---
+++ src/main/java/org/kmuradoff/openschooljavastarter/config/LoggingProperty.java
@@ -0,0 +1,30 @@
+package org.kmuradoff.openschooljavastarter.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "t1.logging")
+public class LoggingProperty {
+    /**
+     * Enable or disable HTTP logging.
+     */
+    private boolean enabled = true;
+
+    /**
+     * Log level: INFO, DEBUG, WARN, ERROR.
+     */
+    private String level = "INFO";
+
+    // Getters and setters
+    public boolean isEnabled() {
+        return enabled;
+    }
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+    public String getLevel() {
+        return level;
+    }
+    public void setLevel(String level) {
+        this.level = level;
+    }
+}
